e0d13e17cd14d21a6a4cd3d47050dc8bb843d0d5
stefanspringer1 <stefan.springer@icloud.com>
2021-07-25 07:20:57 +0200

    made initializer of XMLEventHandlerError public

--- Sources/SwiftXMLInterfaces/XMLInterfaces.swift
+++ Sources/SwiftXMLInterfaces/XMLInterfaces.swift
@@ -10,7 +10,7 @@ public struct XMLEventHandlerError: LocalizedError {
     
     private let message: String
 
-    init(_ message: String) {
+    public init(_ message: String) {
         self.message = message
     }
     
