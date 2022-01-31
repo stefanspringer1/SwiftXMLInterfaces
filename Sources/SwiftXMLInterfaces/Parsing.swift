e60e5794e3712533ebcdf092669c5fa6baf22693
Stefan Springer <stefan.springer@stefanspringer.com>
2022-01-31 11:00:13 +0100

    adjusting to new version of library (from SwiftXMLC)

--- Sources/SwiftXMLInterfaces/Parsing.swift
+++ Sources/SwiftXMLInterfaces/Parsing.swift
@@ -6,107 +6,109 @@
 
 import Foundation
 
-public struct XMLEventHandlerError: LocalizedError {
-    
+public struct XEventHandlerError: LocalizedError {
+
     private let message: String
 
     public init(_ message: String) {
         self.message = message
     }
-    
+
     public var errorDescription: String? {
         return message
     }
 }
 
 public protocol InternalEntityResolver {
-    func resolve(entityName:String, attributeContext: String?, attributeName: String?) -> String?
+    func resolve(entityWithName entityName: String, forAttributeName attributeName: String?, atElement elementName: String?) -> String?
 }
 
-public protocol XMLEventHandler {
-    
+public enum WhitespaceIndicator {
+    case WHITESPACE
+    case NOT_WHITESPACE
+    case UNKNOWN
+}
+
+public protocol XEventHandler {
+
     func documentStart()
-    
+
     func xmlDeclaration(version: String, encoding: String?, standalone: String?)
-    
+
     func documentTypeDeclaration(type: String, publicID: String?, systemID: String?)
-    
-    func elementStart(name: String, attributes: inout [String:String], combineTexts: Bool)
-    
+
+    func elementStart(name: String, attributes: [String:String]?)
+
     func elementEnd(name: String)
-    
-    func text(text: String, isWhitespace: Bool)
-    
+
+    func text(text: String, whitespace: WhitespaceIndicator)
+
     func cdataSection(text: String)
-    
+
     func processingInstruction(target: String, content: String?)
-    
+
     func comment(text: String)
-    
+
     func internalEntityDeclaration(name: String, value: String)
-    
+
     func externalEntityDeclaration(name: String, publicID:  String?, systemID: String)
-    
+
     func unparsedEntityDeclaration(name: String, publicID:  String?, systemID: String, notation: String)
-    
+
     func notationDeclaration(name: String, publicID:  String?, systemID: String?)
-    
+
     func internalEntity(name: String)
-    
+
     func externalEntity(name: String)
-    
-    func elementDeclaration(name: String, text: String)
-    
-    func attributeListDeclaration(name: String, text: String)
-    
+
+    func elementDeclaration(name: String, literal: String)
+
+    func attributeListDeclaration(name: String, literal: String)
+
     func parameterEntityDeclaration(name: String, value: String)
-    
-    func parsingTime(seconds: Double)
-    
+
     func documentEnd()
 }
 
-open class DefaultXMLEventHandler: XMLEventHandler {
-    
+open class DefaultXEventHandler: XEventHandler {
+
     public init() {}
-    
+
     open func documentStart() {}
-    
+
     open func xmlDeclaration(version: String, encoding: String?, standalone: String?) {}
-    
+
     open func documentTypeDeclaration(type: String, publicID: String?, systemID: String?) {}
-    
-    open func elementStart(name: String, attributes: inout [String:String], combineTexts: Bool) {}
-    
+
+    open func elementStart(name: String, attributes: [String:String]?) {}
+
     open func elementEnd(name: String) {}
-    
-    open func text(text: String, isWhitespace: Bool) {}
-    
+
+    open func text(text: String, whitespace: WhitespaceIndicator) {}
+
     open func cdataSection(text: String) {}
-    
+
     open func processingInstruction(target: String, content: String?) {}
-    
+
     open func comment(text: String) {}
-    
+
     open func internalEntityDeclaration(name: String, value: String) {}
-    
+
     open func externalEntityDeclaration(name: String, publicID:  String?, systemID: String) {}
-    
+
     open func unparsedEntityDeclaration(name: String, publicID:  String?, systemID: String, notation: String) {}
-    
+
     open func notationDeclaration(name: String, publicID:  String?, systemID: String?) {}
-    
+
     open func internalEntity(name: String) {}
-    
+
     open func externalEntity(name: String) {}
-    
-    open func elementDeclaration(name: String, text: String) {}
-    
-    open func attributeListDeclaration(name: String, text: String) {}
-    
+
+    open func elementDeclaration(name: String, literal: String) {}
+
+    open func attributeListDeclaration(name: String, literal: String) {}
+
     open func parameterEntityDeclaration(name: String, value: String) {}
-    
-    open func parsingTime(seconds: Double) {}
-    
+
     open func documentEnd() {}
 }
